2f5a35b7dd0e2015ff362264651d82ae8633a955
Greg Pasquariello <greg@pasq.net>
2012-05-05 13:12:08 -0600

    misc checkin

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,7 +2,6 @@ class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @scannerViewController = ScannerViewController.alloc.init
     
-    
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.rootViewController = UINavigationController.alloc.initWithRootViewController(@scannerViewController)
     @window.rootViewController.wantsFullScreenLayout = true
